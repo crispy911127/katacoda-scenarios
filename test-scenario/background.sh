9d93fcfd74d2760161c06b2638ce398f4d26eeec
Christopher Ryan <ryanchris28@gmail.com>
2022-05-15 17:16:36 +0200

    add: additional scenario

---
+++ test-scenario/background.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+docker run --name=scope -d --net=host --pid=host --privileged -v /var/run/docker.sock:/var/run/docker.sock:rw weaveworks/scope:1.9.1 --probe.docker=true
+
+snap install terraform --classic
