8c695c3f8afb667799ac7fe0253819ff03c367a6
Christopher Ryan <ryanchris28@gmail.com>
2022-05-15 11:30:31 +0200

    change: daemonize

--- test-scenario/foreground.sh
+++ test-scenario/foreground.sh
@@ -3,4 +3,4 @@ launch.sh
 # Starting Jenkins
 
 mkdir jenkins_home
-docker run --name jenkins -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:./jenkins_home jenkins/jenkins
+docker run -d --name jenkins -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:./jenkins_home jenkins/jenkins
